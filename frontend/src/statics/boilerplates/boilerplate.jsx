c1a6e4a15e2f472ade7b998960b284191d0b7421
vivek1111a <vivekwadate1111@gmail.com>
2024-01-13 22:47:11 +0530

    datesection 1.1

--- frontend/src/statics/boilerplates/boilerplate.jsx
+++ frontend/src/statics/boilerplates/boilerplate.jsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Navbar from "./navbar";
+import CurrentDate from "./date/currentdate";
 
 export default function Boilerplate() {
   return (
     <>
       <Navbar />
+      <CurrentDate />
     </>
   );
 }
--- frontend/src/statics/boilerplates/date/currentdate.jsx
+++ frontend/src/statics/boilerplates/date/currentdate.jsx
@@ -3,10 +3,16 @@ import "./currentdate.css";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 export default function CurrentDate() {
+  let datenow = new Date();
+  let dateout = datenow.toString().split(" ")[2];
+  console.log();
   return (
     <>
       <div className="datecontainer">
         <CalendarMonthIcon fontSize="large" className="datecontainer-icon" />
+        <p>
+          {datenow.toString().split(" ")[2]} {datenow.toString().split(" ")[1]}
+        </p>
       </div>
     </>
   );
