21a12c58d91e9f423e70d43df251eaa3cad58baf
vivek1111a <vivekwadate1111@gmail.com>
2024-01-15 14:33:26 +0530

    profile-dropdown 1.0

--- frontend/src/statics/boilerplates/navbarcomponents/profile.jsx
+++ frontend/src/statics/boilerplates/navbarcomponents/profile.jsx
@@ -4,9 +4,19 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export default function Profile() {
   return (
-    <div className="navbar-profile">
-      <AccountCircleIcon className="navbar-profile-icon" fontSize="large" />
-      <div className="navbar-profile-name">Profile Name</div>
-    </div>
+    <>
+      <div className="profile-dropdown-container">
+        <div className="navbar-profile">
+          <AccountCircleIcon className="navbar-profile-icon" fontSize="large" />
+          <div className="navbar-profile-name">Profile Name</div>
+        </div>
+        <div className="dropdown-content">
+          <div className="profile-dropdown">Profile</div>
+          <div className="profile-dropdown">language</div>
+          <div className="profile-dropdown">Mode light/dark</div>
+          <div className="profile-dropdown">settings</div>
+        </div>
+      </div>
+    </>
   );
 }
