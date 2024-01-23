4cffa831b065fe0ff41dd17640e9c539591fd091
Swaraj Mali <swarajmali29@gmail.com>
2024-01-23 14:04:20 +0530

    login page 1.1

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,26 +1,52 @@
 import * as React from "react";
+import { useState } from "react";
 import "./login.css"
 import PersonIcon from '@mui/icons-material/Person';
-import EmailIcon from '@mui/icons-material/Email';
 import HttpsIcon from '@mui/icons-material/Https';
 
 export default function Login(){
+  let [FormData, setFormData] = useState({
+    username: "",
+    password: ""
+  });
+
+  let handleInputChange = (event) => {
+    let fieldName = event.target.name;
+    let newValue = event.target.value;
+
+    setFormData((currData) => {
+      currData[fieldName] = newValue;
+      return {...currData};
+    });
+  };
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+
+    setFormData({
+      username: "",
+      password: ""
+    })
+  }
+
   return(
     <>
       <div className="login-container">
         <div className="login-img">
           
         </div>
-        <form action="Login">
+        <form action="Login" onSubmit={handleSubmit}>
           <h1>Login</h1>
+
           <div className="login-input">
             <PersonIcon className="icon" />
-            <input type="text" placeholder="Username"/>
+            <input type="text" placeholder="Username" value={FormData.username} onChange={handleInputChange} name="username"/>
             <hr />
           </div>
+
           <div className="login-input">
             <HttpsIcon className="icon" />
-            <input type="password" placeholder="Password"/>
+            <input type="password" placeholder="Password" name="password" onChange={handleInputChange}/>
             <hr />
           </div>
 
