a1ea86e496b6ab520a9a97b3739fb1794090c54d
vivek1111a <112795819+vivek1111a@users.noreply.github.com>
2024-11-29 13:28:15 +0530

    modified tag object for background color

--- sidebarpages/tracking/Trackingobject.jsx
+++ sidebarpages/tracking/Trackingobject.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getTagArray } from "../../store/tagArraySlice";
 import ProgressBar from "../../boilerplates/progressbar";
 import { useNavigate } from "react-router-dom";
 
 function TrackingObject({ trackingObject }) {
-  const targets = trackingObject.targets;
+  const targets = trackingObject?.targets;
+
+  const currency = useSelector((state) => state.currency.value);
+  const paymentArray = useSelector((state) => state.paymentArray.value);
 
   const month = new Date().getMonth();
   const year = new Date().getFullYear();
@@ -15,16 +18,54 @@ function TrackingObject({ trackingObject }) {
   );
   const navigate = useNavigate();
   const onClick = () => {
-    navigate("/tag/" + trackingObject._id);
+    navigate("/tag/" + trackingObject?._id);
   };
+
+  const thismonth = paymentArray.filter(
+    (payment) =>
+      payment.tagid?._id === trackingObject?._id &&
+      new Date(payment.date).getMonth() === month &&
+      new Date(payment.date).getFullYear() === year
+  );
+  const thisMonthTotal = thismonth.reduce(
+    (acc, payment) => acc + payment.amount,
+    0
+  );
+  const [textColor, setTextColor] = useState("black");
+
+  // Function to determine if a color is light or dark
+  const isColorLight = (color) => {
+    const hex = color.replace("#", "");
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    return brightness > 155;
+  };
+
+  // Update the text color once the color is available
+  useEffect(() => {
+    if (trackingObject?.color) {
+      setTextColor(isColorLight(trackingObject?.color) ? "black" : "white");
+    }
+  }, [trackingObject?.color]);
   return (
-    <div className="trackingobject" key={trackingObject._id} onClick={onClick}>
+    <div
+      className="trackingobject"
+      key={trackingObject._id}
+      onClick={onClick}
+      style={{
+        backgroundColor: trackingObject?.color,
+        cursor: "pointer",
+        color: textColor,
+      }}
+    >
       <div className="trackingobjecttop">
-        <div>{trackingObject.name}</div>
-        <div>This month : 3000</div>
+        <div>{trackingObject?.name}</div>
+        <div>This month : {thisMonthTotal}</div>
       </div>
       <div className="trackingobjectbottom">
-        <div>Type: {trackingObject.tagType}</div>
+        <div>Type: {trackingObject?.tagType}</div>
         <div>Target: {currenttarget ? currenttarget.amount : 0}</div>
       </div>
     </div>
