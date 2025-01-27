91931f964d9e8a76f2ac23f89e1dcaed75720399
vivek1111a <112795819+vivek1111a@users.noreply.github.com>
2025-01-27 18:49:11 +0530

    Refactor footer layout and styling; update sidebar hover effect

--- boilerplates/footer.jsx
+++ boilerplates/footer.jsx
@@ -1,44 +1,25 @@
-import React from "react"
-import "./footer.css"
+import React from "react";
+import "./footer.css";
 
-export default function footer(){
-  return(
+export default function footer() {
+  return (
     <div className="footer">
-      <div className="container">
-        <div className="row">
-          {/* column 1 */}
-          <div className="column">
-            <h4>RupeeRise</h4>
-            <ul className="list">
-              <li>4206969</li>
-              <li>Mumbai, India</li>
-              <li>221B, Baker Street</li>
-            </ul>
-          </div>
-          <div className="column">
-            <h4>Something</h4>
-            <ul className="list">
-              <li>Memes</li>
-              <li>Co-Founder</li>
-              <li>CEO</li>
-            </ul>
-          </div>
-          <div className="column">
-            <h4>Something</h4>
-            <ul className="list">
-              <li>Memes</li>
-              <li>Co-Founder</li>
-              <li>CEO</li>
-            </ul>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <p className="col-sm">
-            &copy;{new Date().getFullYear()} RupeeRise | All Rights Reserved | Terms of Service
-          </p>
-        </div>
+      <div className="footer-col">
+        <h1 className="footer-col-title">Links</h1>
+        <p className="footer-col-container">Login</p>
+        <p className="footer-col-container">Pricing</p>
+        <p className="footer-col-container">Support</p>
+        <p className="footer-col-container">FAQ</p>
+      </div>
+      <div className="footer-col">
+        <h1 className="footer-col-title">Contact</h1>
+        <p className="footer-col-container">Email:</p>
+      </div>
+      <div className="footer-col">
+        <h1 className="footer-col-title">Legal</h1>
+        <p className="footer-col-container">Terms of Service</p>
+        <p className="footer-col-container">Privacy Policy</p>
       </div>
     </div>
-  )
+  );
 }
